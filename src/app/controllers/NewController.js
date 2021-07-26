997b7bb03368fe8f4bd32b7fc528c63fe36f1bfd
SonNguyen <sonnguyenprofessor@gmail.com>
2021-07-26 21:54:10 +0700

    MVC controller

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,15 @@
+class NewController {
+
+    // [GET] /news
+    index(req, res){
+        res.render('news');
+    }
+
+    //[GET] /news/:slug
+    show(req, res) {
+        res.send('OKE r nhá baby !!! <3')
+    }
+
+}
+
+module.exports =new NewController;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const path = require('path');
 const app = express();
 const port = 3000
 
+const routes = require('./routes');
+const route = require('./routes');
+
 //Static file cho img băng cao độ của các file ngoài ( vượt qua thư mục public)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP loger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))  
 // Template engine
 app.engine('hbs', handlebars({
   extname: 'hbs'
@@ -20,9 +23,8 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 // console.log('PATH: ', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// khởi tạo tuyến đường
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
