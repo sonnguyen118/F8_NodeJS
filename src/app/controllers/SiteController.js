034c4438bfb266af2d14ac12536417c0880c5288
SonNguyen <sonnguyenprofessor@gmail.com>
2021-07-27 22:54:53 +0700

    bài 25

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,21 +1,22 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject} = require('../../util/mongoose')
 
 class SiteController {
 
     // [GET] /
-    index(req, res) {
-        Course.find({}, function(err, courses) {
-            if(!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({error: 'ERROR!!!!!'});
-            }
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses)
+                });
+            })
+            .catch(next);
     }
-    // [GET] /
-    index(req, res) {
-        res.render('home');
-    }
+    // // [GET] /
+    // index(req, res) {
+    //     res.render('home');
+    // }
 
     //[GET] /search
     search(req, res) {
